59402fe3a29c3c1c070af76dd4d4aba1f0459a0b
Joshua-Kemp <jmkemp@pronto.las.iastate.edu>
2023-04-25 10:57:54 -0500

    changed to enseble refernce and annotation to improve stringtie performance

--- LCM_dataset_alignment.sh
+++ LCM_dataset_alignment.sh
@@ -7,8 +7,14 @@
 
 #build index with hisat2
 acclist=LCM_SRA_ACC_list.txt  #list of SRA accesions to download and map
-reference_genome_url="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/735/GCF_000001735.4_TAIR10.1/GCF_000001735.4_TAIR10.1_genomic.fna.gz"
-reference_annotation_url="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/735/GCF_000001735.4_TAIR10.1/GCF_000001735.4_TAIR10.1_genomic.gtf.gz"
+reference_genome_url="https://ftp.ebi.ac.uk/ensemblgenomes/pub/release-56/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz"
+reference_annotation_url="https://ftp.ebi.ac.uk/ensemblgenomes/pub/release-56/plants/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.56.gtf.gz"
+https://ftp.ebi.ac.uk/ensemblgenomes/pub/release-56/plants/gtf/arabidopsis_thaliana
+https://ftp.ebi.ac.uk/ensemblgenomes/pub/release-56/plants/fasta/arabidopsis_thaliana/dna/
+
+#Note: If using the following NCBI refences and gtf annotation, there will be issues downstream for stringtie I have no clue if the original paper used a masked or unmasked version of the genome etc
+#https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/735/GCF_000001735.4_TAIR10.1/GCF_000001735.4_TAIR10.1_genomic.fna.gz
+#https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/735/GCF_000001735.4_TAIR10.1/GCF_000001735.4_TAIR10.1_genomic.gtf.gz
 
 splitn=2 #  number of sbatchs to split accession file into.  Makes running faster if there are many accessions in the list
 
@@ -37,8 +43,8 @@ cat << 'EOF' > download_trim_and_map.sh  #this is a readdoc notation.  It prints
 #variables
 basepath=$(pwd)  # finds the current directory so that the program can use complete path names to specify files.  file name variables can be updated to use relative paths if needed.
 genome="GCF_*.fna"  #Make sure this matches after indexing
-threads=16
-memory=100  #memory in gb bbduk is allowed to use
+threads=24
+memory=200  #memory in gb bbduk is allowed to use
 prefetch_pipe=./prefetch_tmp/${1}
 fastq_1=${basepath}/downloaded_reads/${1}_1.fastq
 fastq_2=${basepath}/downloaded_reads/${1}_2.fastq
@@ -48,17 +54,18 @@ out=${basepath}/alignments/${1}.bam
 
 prefetch $1 --max-size 80g -O prefetch_tmp  # downloads a compressed version of the raw reads.  Max size must be increased since the files are larger (~24gb) than the preset maximum of 20gb
 fasterq-dump -f --threads ${threads} --split-3 --outdir downloaded_reads ${prefetch_pipe} &&  #this will decompress the reads into 3 files (split 3 option) pair 1 pair 2 and unpaired reads.  It appears the unpaired reads were not uploaded to the database as there are none when complete
-fastqc -t ${threads} -o pretrim_quality $fastq_1 & # will show the quality data for the reads pre trimming
-fastqc -t ${threads} -o pretrim_quality $fastq_2 &
-bbduk.sh -Xmx${memory}g in1=${fastq_1} in2=${fastq_2} out1=${trimmed_1} out2=${trimmed_2} threads=${threads} ref=adapters.fa minlen=50 qtrim=r trimq=15 k=23 ktrim=r hdist=1 mink=11 tpe tbo &&  # see https://jgi.doe.gov/data-and-tools/software-tools/bbtools/bb-tools-user-guide/bbduk-guide/ for details on the settings of this trimmer
+fastqc -t ${threads} -o pretrim_quality $fastq_1 && # will show the quality data for the reads pre trimming
+fastqc -t ${threads} -o pretrim_quality $fastq_2 &&
+bbduk.sh -Xmx${memory}g in1=${fastq_1} in2=${fastq_2} out1=${trimmed_1} out2=${trimmed_2} threads=${threads} ref=adapters minlen=50 qtrim=r trimq=15 k=23 ktrim=r hdist=1 mink=11 tpe tbo &&  # see https://jgi.doe.gov/data-and-tools/software-tools/bbtools/bb-tools-user-guide/bbduk-guide/ for details on the settings of this trimmer
 fastqc -t ${threads} -o posttrim_quality $trimmed_1 &
 fastqc -t ${threads} -o posttrim_quality $trimmed_2 &
+wait
 hisat2 -p ${threads} --dta -x ${genome} -1 ${trimmed_1} -2 ${trimmed_2} -S ${out}  #hisat2 will align the read pairs to the reference genome.  Default settings were used in the original paper.   I added --dta to so that the output is better for stringtie. Note that hisat2 is an aligner meant for rna unlike bwa-mem to allow for splicing
 
 wait
 rm -r prefetch_tmp
 
-    exit
+
 EOF
 
 
@@ -93,11 +100,11 @@ cat << 'EOF' > align_slurmheader.txt
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=0-4:00:00  # max job runtime
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=16  # number of processor cores
 #SBATCH --nodes=1  # number of nodes
 #SBATCH --partition=biocrunch  # partition(s)
-#SBATCH --mem=1200G  # max memory
+#SBATCH --mem=800G  # max memory
 #SBATCH -J "LCM_rnaSeq align"  # job name
 
 EOF
@@ -106,11 +113,11 @@ cat << 'EOF' > readcount_slurmheader.txt
 #!/bin/bash
 
 #SBATCH --time=0-2:00:00  # max job runtime
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=16  # number of processor cores
 #SBATCH --nodes=1  # number of nodes
 #SBATCH --partition=biocrunch  # partition(s)
-#SBATCH --mem=1200G  # max memory
+#SBATCH --mem=800G  # max memory
 #SBATCH -J "LCM_readcount"  # job name
 
 EOF
@@ -123,7 +130,7 @@ cat << 'EOF' > slurmheader_index.txt
     
 #SBATCH --time=0-00:15:00  # max job runtime
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16  # number of processor cores
+#SBATCH --cpus-per-task=24  # number of processor cores
 #SBATCH --nodes=1  # number of nodes
 #SBATCH --partition=biocrunch  # partition(s)
 #SBATCH --mem=600G  # max memory
@@ -160,15 +167,25 @@ echo "module load gzip" >> index_genome.batch
 echo "module load hisat2" >> index_genome.batch
 echo "wget ${reference_genome_url}" >> index_genome.batch &&
 echo "wget ${reference_annotation_url}" >> index_genome.batch &&
-echo "genome=*GCF_*.fna.gz" >> index_genome.batch
-echo "annotation=*GCF_*.gtf.gz" >> index_genome.batch
+echo "genome=*.fa.gz" >> index_genome.batch
+echo "annotation=*.gtf.gz" >> index_genome.batch
 echo "gzip -d \$genome" >> index_genome.batch
 echo "gzip -d \${annotation}" >> index_genome.batch
-echo "genome=*GCF_*.fna" >> index_genome.batch
-echo "annotation=*GCF_*.gtf" >> index_genome.batch
+echo "genome=*.fa" >> index_genome.batch
+echo "annotation=*.gtf" >> index_genome.batch
 echo "hisat2_extract_splice_sites.py \${annotation} > genome_splice_sites" >> index_genome.batch
 echo "hisat2_extract_exons.py \${annotation} > genome_exons" >> index_genome.batch # extract exons from gtf file so hisat may add them to the index and use them to inform the alignment
 echo "hisat2-build  --exon genome_exons --ss genome_splice_sites \$genome \$genome" >> index_genome.batch #extract splice sites from gtf file so hisat may add them to the index and use them to inform the alignment
+
+
+#there are unlabeled features in the NCBI gtf file which cause problems for stringtie.  A side effect of how NCBI currently process certain things apparently.  The following should remove those
+#There is also trans-splicing gene and at least one other transcript for which strand is not assigned, (?), which also causes problems for string tie.  The following greps everything without one of the problem question marks.  This removes almost 40000 lines and is probably a little heavy handed.  I'll test an awk script that looks just at that column or I'll just repeat everything using the ENSEMBL reference and annotation.
+#cat << 'EOF' >> index_genome.batch
+#grep -P '\btranscript_id\s+"[^"]+"' GCF_000001735.4_TAIR10.1_genomic.gtf > fixed_GCF_000001735.4_TAIR10.1_genomic.gtf
+#grep -ve '?' fixed_GCF_000001735.4_TAIR10.1_genomic.gtf > fixed_twice_GCF_000001735.4_TAIR10.1_genomic.gtf
+#EOF
+
+
 
 ## create n files dividing up the accesions into managagable chunks for each slurm batch
 split -n l/${splitn} ../$acclist piece_acc_
